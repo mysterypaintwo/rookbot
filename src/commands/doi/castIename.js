e896eca1baaffc77785f764056aa44eeed7de824
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-11-25 20:02:50 -0800

    OOP: Castlename

--- src/commands/doi/castIename.js
+++ src/commands/doi/castIename.js
@@ -1,78 +1,76 @@
-const { RookEmbed } = require('../../classes/embed/rembed.class');
+const { RookCommand } = require('../../classes/command/rcommand.class');
 const { changeNickname } = require('../../utils/changeNickname');  // Import the changeNickname function
 
-module.exports = {
-  execute: async (client, interaction) => {
-    const guildID = interaction.guild.id;
-    const userID = '1111517386588307536';
-    const doiGuildID = 1282788953052676177;
-    const isDoI = interaction.guild.id === doiGuildID;
+module.exports = class CastleNameCommand extends RookCommand {
+  constructor() {
+    let comprops = {
+      name: "castlename",
+      description: "Immediately triggers a nickname change for castle, to a random castle-esque name",
+      // permissionsRequired: [],
+      botPermissions: ["MANAGE_NICKNAMES"]
+    }
+    let props = {}
+    super(
+      {...comprops},
+      {...props}
+    )
+  }
 
+  async action(client, interaction) {
     // Ensure the command is properly deferred and acknowledged with ephemeral response
     await interaction.deferReply({ ephemeral: true });
-    
+
+    // Get this Guild ID
+    // Set Castle's User ID
+    // Set DoI Guild ID
+    // Determine if we're in DoI
+    const guildID = interaction.guild.id;
+    const userID = "1111517386588307536";
+    const doiGuildID = "1282788953052676177";
+    const isDoI = interaction.guild.id === parseInt(doiGuildID);
+
     try {
+      // Get this guild
       const guild = await client.guilds.fetch(guildID);
       if (!guild) {
-        throw new Error("Guild not found.");
+        this.error = true
+        this.props.description = `Guild not found [${guildID}]`
       }
-        
+
+      // Find Castle
       const member = await guild.members.fetch(userID, { force: true }).catch(err => {
-        console.error("Fetch error:", err);
+        this.error = true
+        this.props.description = `Fetch error [${userID}]: ${err}`
       });
-            
+
+      // Couldn't find Castle
       if (!member || !member.user) {
+        this.error = true
+        this.props.description = "Member not found or invalid data"
         throw new Error("Member not found or invalid data.");
       }
 
       // Call the utility function to change the nickname
       const result = await changeNickname(client, member, isDoI);
-    
-      let players = {}
-      players["user"] = {
-        name: interaction.user.displayName,
-        avatar: interaction.user.avatarURL(),
-        username: interaction.user.username
-      }
-      players["target"] = {
-        name: member.displayName,
-        avatar: member.user.displayAvatarURL()
-      }
-      
+
       if (result.success) {
-        const props = {
-          title: {
-            text: "Nickname Changed",
-          },
-          description: result.message,
-          players: players
-        };
-        const embed = new RookEmbed(props);
-        await interaction.editReply({ ephemeral: true, embeds: [embed] });
+        this.props.players.target = {
+          name: member.user.displayName,
+          avatar: member.user.avatarURL()
+        }
+        this.props.title = { text: "Nickname Changed" }
+        this.props.description = result.message
         console.log(result.message);
       } else {
-        const errorProps = {
-          color: "#FF0000",
-          title: { text: "Error" },
-          description: result.message,
-        };
-        const embed = new RookEmbed(errorProps);
-        await interaction.editReply({ ephemeral: true, embeds: [embed] });
+        this.error = true
+        this.props.description = result.message
       }
     } catch (error) {
-      let props = {
-        color: "#FF0000",
-        title: { text: "Error" },
-        description: "There was an error changing the nickname.",
-      };
-      const embed = new RookEmbed(props);
-      await interaction.editReply({ ephemeral: true, embeds: [embed] });
+      this.error = true
+      this.props.description = "There was an error changing the nickname"
       console.error("Error changing nickname:", error);
     }
-  },
 
-  name: 'castlename',
-  description: 'Immediately triggers a nickname change for castle, to a random castle-esque name.',
-  //permissionsRequired: ['MANAGE_NICKNAMES'],
-  botPermissions: ['MANAGE_NICKNAMES'],
+    await interaction.deleteReply();
+  }
 };
