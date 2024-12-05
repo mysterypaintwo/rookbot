2e23ad9a2f8cff7dad67f7c6f7e598c5940154f9
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-12-05 00:03:31 -0800

    GHA: Avoid instantiation

--- src/index.js
+++ src/index.js
@@ -24,9 +24,18 @@ const client = new Client(
 
   if (process.env.GITHUB_WORKFLOW) {
     console.log(process.env.GITHUB_WORKFLOW)
-    setTimeout(() => {
-      let exit_cmd = new ExitCommand()
-      exit_cmd.execute(client)
+    setTimeout(async () => {
+      const getLocalCommands = require('../../utils/getLocalCommands')
+      const localCommands = getLocalCommands()
+      try {
+        let commandName = "ExitCommand"
+        const commandObject = localCommands.find(
+          (cmd) => cmd.name === commandName
+        )
+        await commandObject.execute(client, interaction);
+      } catch(err) {
+        console.log(err.stack)
+      }
     })
   }
 })()
